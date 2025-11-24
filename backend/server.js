bf07befe24de54b6424ce9f9b303456a902f7524
krishankraghuvanshi <krishank.raghuvanshi@gmail.com>
2025-11-25 00:54:35 +0530

    chat application HomePage and ChatPage routes implemented in app along with backend logic

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const {chats} = require("./data/data")
+const dotenv = require("dotenv")
+const PORT = process.env.PORT || 5782
+dotenv.config()
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("api is running sucessfully")
+})
+
+app.get("/api/chat", (req, res) => {
+    res.send(chats)
+})
+
+app.get("/api/chat/:id", (req, res) => {
+    const singleChat = chats.find((c) => c._id == req.params.id);
+    res.send(singleChat);
+})
+
+app.listen(5782, console.log(`server is running on port {5782}`))
